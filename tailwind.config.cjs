dcc6e211e1123e711d3e48519c6ea9a82b8caaa0
Gauthier Souquet <gauthier.souquet@etu.umontpellier.fr>
2025-05-28 12:11:46 +0200

    changement de da et correction de bug d'image

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,5 @@
 const plugin = require('tailwindcss/plugin');
+const colors = require('tailwindcss/colors');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -12,31 +13,28 @@ module.exports = {
 			lg: '1024px',
 			xl: '1280px',
 		},
-		colors: {
-			black: '#000',
-			white: '#fff',
-			orange: '#E06330',
-		},
-		fontFamily: {
-			// Headings
-			sans: ['Inter', 'sans-serif'],
-			// Base text
-			monospace: ['Inconsolata', 'monospace'],
-		},
-		fontSize: {
-			xs: '.75rem',
-			sm: '.875rem',
-			tiny: '.875rem',
-			base: '1rem',
-			lg: '1.125rem',
-			xl: '1.25rem',
-			'2xl': '1.5rem',
-			'3xl': '1.875rem',
-			'4xl': '2.25rem',
-			'5xl': '3rem',
-		},
-		letterSpacing: {
-			wide: '.025em',
+		extend: {
+			colors: {
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],            // Police pour titres et textes sans-serif
+				monospace: ['Inconsolata', 'monospace'], // Police pour monospace
+			},
+			fontSize: {
+				xs: '.75rem',
+				sm: '.875rem',
+				tiny: '.875rem',
+				base: '1rem',
+				lg: '1.125rem',
+				xl: '1.25rem',
+				'2xl': '1.5rem',
+				'3xl': '1.875rem',
+				'4xl': '2.25rem',
+				'5xl': '3rem',
+			},
+			letterSpacing: {
+				wide: '.025em',
+			},
 		},
 	},
 	plugins: [
